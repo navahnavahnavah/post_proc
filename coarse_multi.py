96dc357f2bae9ece22b79af17c576c71a1d867e1
Navah Farahat <navahnavahnavah@gmail.com>
2017-09-17 16:32:57 -0500

    coarse multi and chamber toggle

---
+++ coarse_multi.py
@@ -0,0 +1,56 @@
+#coarse_multi.py
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import math
+import streamplot as sp
+import multiplot_data as mpd
+import heapq
+import os.path
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.patches import Patch
+import matplotlib.ticker as ticker
+plt.rcParams['contour.negative_linestyle'] = 'solid'
+plt.rc('font', family='Arial')
+plt.rc('xtick', labelsize=7)
+plt.rc('ytick', labelsize=7)
+plt.rcParams['axes.titlesize'] = 10
+#plt.rcParams['hatch.linewidth'] = 0.1
+
+plt.rcParams['axes.color_cycle'] = "#CE1836, #F85931, #EDB92E, #31aa22, #04776b"
+
+col = ['#880000', '#ff0000', '#ff7411', '#bddb00', '#159600', '#00ffc2', '#0000ff', '#2f3699','#8f00ff', '#ec52ff', '#6e6e6e', '#000000', '#c6813a', '#7d4e22', '#ffff00', '#df9a00', '#812700', '#6b3f67', '#0f9995', '#4d4d4d', '#00530d', '#d9d9d9', '#e9acff']
+
+secondary = np.array(['', 'kaolinite', 'saponite_mg', 'celadonite', 'clinoptilolite', 'pyrite', 'mont_na', 'goethite',
+'smectite', 'calcite', 'kspar', 'saponite_na', 'nont_na', 'nont_mg', 'fe_celad', 'nont_ca',
+'mesolite', 'hematite', 'mont_ca', 'verm_ca', 'analcime', 'philipsite', 'mont_mg', 'gismondine',
+'verm_mg', 'natrolite', 'talc', 'smectite_low', 'prehnite', 'chlorite', 'scolecite', 'clinochlorte14a',
+'clinochlore7a', 'saponite_ca', 'verm_na', 'pyrrhotite', 'fe_saponite_ca', 'fe_saponite_mg', 'daphnite7a', 'daphnite14a', 'epidote'])
+
+primary = np.array(['', '', 'plagioclase', 'pyroxene', 'olivine', 'basaltic glass'])
+
+density = np.array([0.0, 2.65, 2.3, 3.05, 2.17, 5.01, 2.5, 3.8,
+2.7, 2.71, 2.56, 2.3, 2.28, 2.28, 3.05, 2.28,
+2.25, 5.3, 2.5, 2.55, 2.27, 2.2, 2.5, 2.26,
+2.55, 2.25, 2.75, 2.7, 2.87, 2.9, 2.275, 2.8,
+2.8, 2.3, 2.55, 4.61, 2.3, 2.3, 3.2, 3.2, 3.45])
+
+molar = np.array([0.0, 258.156, 480.19, 429.02, 2742.13, 119.98, 549.07, 88.851,
+549.07, 100.0869, 287.327, 480.19, 495.90, 495.90, 429.02, 495.90,
+380.22, 159.6882, 549.07, 504.19, 220.15, 649.86, 549.07, 649.86,
+504.19, 380.22, 379.259, 549.07, 395.38, 64.448, 392.34, 64.448,
+64.448, 480.19, 504.19, 85.12, 480.19, 480.19, 664.0, 664.0, 519.0])
+
+molar_pri = np.array([277.0, 153.0, 158.81, 110.0])
+
+density_pri = np.array([2.7, 3.0, 3.0, 3.0])
+
+batch_path = "../output/revival/summer_coarse_grid/"
+batch_path_ex = "../output/revival/summer_coarse_grid/td_4e10"
+
+x = np.loadtxt(batch_path_ex + 'x.txt',delimiter='\n')
+y = np.loadtxt(batch_path_ex + 'y.txt',delimiter='\n')
+
+param_t_diff = np.array([4e10, 1e10, 5e9])
+param_t_diff_string = ['4e10' , '1e10' , '5e9']
